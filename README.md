701db6bb5a4e34fd6f4604206591e2dfbc3581dc
Gabriel Dupim <gabrdupim@gmail.com>
2021-04-07 16:56:09 -0300

    adiciona código HTML index.html e pasta js com código JavaScript main.js e também um README.md

---
+++ README.md
@@ -0,0 +1,6 @@
+# DIO - Curso - Programação para Internet com JavaScript
+
+Este é o repositório do curso da *Digital Innovation One* sobre "programação para internet com JavaScript".
+
+***Todos os exercícios deste curso estarão aqui armazenados e catalogados.***
+
---
+++ js/main.js
@@ -0,0 +1,20 @@
+var nome = "Gabriel Dupim";
+//var idade = 16;
+//var idade2 = 10;
+var n1 = 5;
+var n2 = 3;
+var frase = "Brasil é o melhor time do mundo.";
+//var idade = "16";
+//var idade2 = "10";
+
+//alert("Bem-vindo " + nome);
+//alert(nome + " tem idade de " + idade + " anos.");
+//alert(idade + idade2);
+console.log(nome);
+//console.log(idade + idade2);
+console.log(n1 * n2);
+//console.log(n1 * idade);
+console.log(frase.replace("Brasil", "Alemanha"));
+console.log(frase.toUpperCase());
+console.log(frase.toLowerCase());
+//alert(frase.replace("Brasil", "Japão"));
