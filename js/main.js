519575344f959633b2e18d720c9180e60d528c7a
Gabriel Dupim <gabrdupim@gmail.com>
2021-04-07 17:50:41 -0300

    adiciona mais código e uma descrição deles ao main.js

--- js/main.js
+++ js/main.js
@@ -1,20 +1,48 @@
+var frutas = [{nome: "Maçã", cor: "Vermelha"}, {nome: "Uva", cor: "Roxa"}];
+console.log(frutas);
+alert(frutas[1].nome);
+
+/* Aula 2:
+var fruta = {nome: "Maçã", cor: "Vermelha"}; - Cria um dicionário.
+console.log(fruta); - Mostra o conteúdo do dicionário.
+console.log(fruta.nome); - Mostra o nome declarado dentro do dicionário.
+alert(fruta.cor); - Mostra a cor declarada dentro do dicionário.
+
+var lista = ["Maçã", "Pêra", "Laranja"]; - Adiciona lista.
+lista.push("Uva"); - Adiciona valor a lista.
+console.log(lista); - Mostra o conteúdo da lista.
+lista.pop(); - Remove o último valor da lista.
+console.log(lista[0]); - Imprime um valor da lista.
+console.log(lista.toString()[0]); - Imprime, no caso aqui, a primeira letra do primeiro valor.
+console.log(lista.join(" - ")); - Adiciona um "-" entre os valores para o .toString();
+console.log(lista.join(" | "));
+console.log(lista.join(" "));
+console.log(lista.join(""));
+console.log(lista.toString()); - Converte a lista em um texto.
+console.log(lista.length); - Imprime o tamanho da lista.
+console.log(lista.reverse()); - Inverte os valores da lista.
+alert(lista[1]);
+*/
+
+/* Aula 1:
 var nome = "Gabriel Dupim";
-//var idade = 16;
-//var idade2 = 10;
+var idade = 16;
+var idade2 = 10;
 var n1 = 5;
 var n2 = 3;
 var frase = "Brasil é o melhor time do mundo.";
-//var idade = "16";
-//var idade2 = "10";
+var idade = "16";
+var idade2 = "10";
 
-//alert("Bem-vindo " + nome);
-//alert(nome + " tem idade de " + idade + " anos.");
-//alert(idade + idade2);
+alert("Bem-vindo " + nome);
+alert(nome + " tem idade de " + idade + " anos.");
+alert(idade + idade2);
 console.log(nome);
-//console.log(idade + idade2);
+console.log(idade + idade2);
 console.log(n1 * n2);
-//console.log(n1 * idade);
-console.log(frase.replace("Brasil", "Alemanha"));
-console.log(frase.toUpperCase());
-console.log(frase.toLowerCase());
-//alert(frase.replace("Brasil", "Japão"));
+console.log(n1 * idade);
+console.log(frase.replace("Brasil", "Alemanha")); - Muda um valor dentro de uma varíavel.
+alert(frase.replace("Brasil", "Japão")); 
+console.log(frase.toUpperCase()); - Faz com que a frase da varíavel fique em maiúsculas.
+console.log(frase.toLowerCase()); - Faz com que a frase da varíavel fique em minúsculas.
+*/
